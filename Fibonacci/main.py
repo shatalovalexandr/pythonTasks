e3374fb23b3b5a96cfdca4fc2f2c85952031c4f3
Alexandr Shatalov <alex.shat.85@gmail.com>
2022-05-15 17:03:59 +0600

    Fibonacci

---
+++ Fibonacci/main.py
@@ -0,0 +1,19 @@
+def fib(n):
+    n1 = 0
+    n2 = 1
+    fibonacci = 0
+    if n == n1 or n == n2:
+        return n
+    else:
+        for i in range(n):
+            fibonacci = n1 + n2
+            n1, n2 = fibonacci, n1
+        return fibonacci
+
+
+if __name__ == '__main__':
+    n = input('Input int num:')
+    if type(int(n)) is int:
+        print(fib(int(n)))
+    else:
+        print('Num must be type `int`')
